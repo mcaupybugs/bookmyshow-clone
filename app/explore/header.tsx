2a75caf869247409ac42c8b5724100fe874b310a
Vishal Yadav <vishalydv.me@gmail.com>
2023-12-25 02:03:25 +0530

    adding update

--- app/explore/header.tsx
+++ app/explore/header.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import SearchBar from './searchBar';
+import '../globals.css'
 
 const Header = () => {
     return (
-        <h1>Header</h1>
+        <div className='ml-20 mt-5 mr-20 grid grid-col-5'>
+            <div className='w-32 h-12 col-start-1 border-2'>
+                <img src='bms-logo.png'></img>
+            </div>
+            <div className='col-start-2 col-end-4 border-2'>
+                <SearchBar></SearchBar>
+            </div>
+            <div className='col-start-4 col-end-5 border-2'>
+            </div>
+            <div className='col-start-5 border-2'>
+            </div>
+        </div>
     )
 }
 
---
+++ app/explore/searchBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../globals.css'
+
+const SearchBar = () =>{
+    return (
+        <input className='border-2 h-12 w-80' type='search'></input>
+    )
+}
+
+export default SearchBar;
