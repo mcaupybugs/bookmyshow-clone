3ac5519f1669b88215d650514c73eec47985a2f5
Vishal Yadav <vishalydv.me@gmail.com>
2024-01-02 03:59:56 +0530

    Adding latest update

--- app/explore/header.tsx
+++ app/explore/header.tsx
@@ -8,20 +8,20 @@ import Drawer from './drawer';
 
 const Header = () => {
     return (
-        <div className='flex flex-row'>
-            <div className='basis-1/6'>
-                <Image src='/bms-logo.png' alt='Using bms logo' width={200} height={100}/>
+        <div className='flex flex-row mx-14 border-2 h-20'>
+            <div className='basis-2/12 border-2 h-20'>
+                <Image src='/bms-logo.png' alt='Using bms logo' width={120} height={30}/>
             </div>
-            <div className='basis-2/6'>
+            <div className='basis-7/12 border-2 h-20'>
                 <SearchBar></SearchBar>
             </div>
-            <div className='basis-1/6'>
+            <div className='basis-1/12'>
                 <Location></Location>
             </div>
-            <div className='basis-1/6'>
+            <div className='basis-1/12'>
                 <Login></Login>
             </div>
-            <div className='basis-1/6'>
+            <div className='basis-1/12'>
                 <Drawer></Drawer>
             </div>
         </div>
--- app/explore/searchBar.tsx
+++ app/explore/searchBar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../globals.css'
-import styles from './searchBar.module.css'
+import Image from 'next/image'
 
 const SearchBar = () =>{
     return (
-        <input className={styles.searchbar} type='search'></input>
+        <div className='container'>
+            <div className='flex flex-row'>
+                <Image className='basis-1/12 w-4 h-4' src='/search.png' alt='searchicon' width={10} height={20}></Image>
+                <input className='w-80 h-8 m-2 border rounded basis-11/12' type='search'></input>
+            </div>
+        </div>
     )
 }
 
